6625e2bab1f74f6799847a2af6c9daeda0f0bc1d
Flavio Gouvea <46804396+freitasgouvea@users.noreply.github.com>
2019-07-11 16:35:36 -0300

    Create SociedadeEmpresarial.sol

---
+++ SociedadeEmpresarial.sol
@@ -0,0 +1,107 @@
+pragma solidity 0.5.1.;
+
+contract SociedadeEmpresarial {
+    address payable owner;
+    string nomeDaSociedade;
+    uint256 cnpj;
+    uint256 capitalSocial;
+    uint256 valorDaAcao;
+    uint256 numeroDeAcoes;
+
+    acionista [] listaDeAcionistas;
+    //administrador [] listaDeAdministradores;
+    //conselheiroFiscal [] listaDeConselheiros;
+    alteracaoDoContratoSocial [] listeDeAlteracoesDoContratoSocial;
+    //assembleiaGeral [] listaDeAssembleias;
+    //resultadoFinanceiro [] listaDeResultados;
+    alienacao [] listaDeAlienacoes;
+    
+    struct acionista {
+        string nomeAcionista;
+        uint256 cpfAcionista;
+        address payable acionistaWallet;
+        uint256 numeroDeAcoes;
+        bool acionistaAtivo;
+    }
+    
+    //struct administrador
+    
+    //struct conselhoFiscal
+    
+    //struct assembleiaGeral
+    
+    struct alteracaoDoContratoSocial {
+        bool votacaoEncerrada;
+        bool resultadoDaVotacao;
+        uint256 capitalSocialAnterior;
+        uint256 novoCapitalSocial;
+        uint256 numeroDeAcoesAnterior;
+        uint256 novoNumeroDeAcoes;
+        uint256 dataDaOperacao;
+    }
+    
+    //struct assembleiaGeral
+    
+    //struct resultadoFinanceiro
+    
+    struct alienacao {
+        address payable vendedorWallet;
+        address payable compradorWallet;
+        uint256 quantidadeDeAcoes;
+        uint256 prazoParaAOperacao;
+        uint256 valorDaOperacao;
+        uint256 dataDaOperacao;
+    }
+    
+    constructor (string memory _nomeDaSociedade, uint256 _cnpj) public {
+        owner =  msg.sender;
+        nomeDaSociedade = _nomeDaSociedade;
+        cnpj = _cnpj;
+    }
+    
+    function registrarContratoSocial( uint256 _capitalSocial, uint256 _numeroDeAcoes, uint256 _valorDaAcao) public {
+        owner =  msg.sender;
+        capitalSocial = _capitalSocial;
+        numeroDeAcoes = _numeroDeAcoes;
+        valorDaAcao = _valorDaAcao;
+    }
+    
+    //function alterarContratoSocial()
+    
+    //function votarAlteracaoDoContratoSocial()
+    
+    function registrarAcionista(string memory _nome, uint256 _cpf, address payable _wallet, uint256 _numeroDeAcoes) public {
+        require (msg.sender == owner);
+        listaDeAcionistas.push(acionista(_nome, _cpf, _wallet, _numeroDeAcoes, true));
+    }
+    
+    //function distribuirAcoes()
+    
+    //function assembleiaGeral ()
+    
+    //function votarAssembleiaGeral()
+    
+    //function registrarResultado()
+    
+    //function aprovarResultadoFinanceiro()
+    
+    //function distribuirDividendos()
+    
+    function autorizarAlienacao(uint256 _idVendedor, uint256 _idComprador, uint256 _acoesAlienadas, uint256 _prazoParaAOperacao) public {
+        require (msg.sender == listaDeAcionistas[_idVendedor].acionistaWallet);
+        require (_acoesAlienadas <= listaDeAcionistas[_idVendedor].numeroDeAcoes);
+        listaDeAlienacoes.push(alienacao(listaDeAcionistas[_idComprador].acionistaWallet, listaDeAcionistas[_idVendedor].acionistaWallet, _acoesAlienadas, _acoesAlienadas*valorDaAcao, _prazoParaAOperacao, 0 ));
+    }
+    
+    function pagarAlienacao(uint256 _idAlienacao, uint256 _idVendedor, uint256 _idComprador) public payable {
+        require (msg.sender == listaDeAlienacoes[_idAlienacao].compradorWallet);
+        require (now <= listaDeAlienacoes[_idAlienacao].prazoParaAOperacao);
+        require (msg.value == listaDeAlienacoes[_idAlienacao].valorDaOperacao);
+        listaDeAcionistas[_idVendedor].numeroDeAcoes -= listaDeAlienacoes[_idAlienacao].quantidadeDeAcoes;
+        listaDeAcionistas[_idComprador].numeroDeAcoes += listaDeAlienacoes[_idAlienacao].quantidadeDeAcoes;
+        listaDeAlienacoes[_idAlienacao].vendedorWallet.transfer(msg.value);
+        listaDeAlienacoes[_idAlienacao].dataDaOperacao = now;
+    }
+    
+    // views
+}
